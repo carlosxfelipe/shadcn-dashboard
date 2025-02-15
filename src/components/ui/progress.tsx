6dc1f5aefc3f6e5e61223d7213e8e5e49f6347ee
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-02-14 21:06:28 -0300

    feat: add progress page and update dashboard layout with progress link

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,10 +5,16 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface CustomProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorColor?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  CustomProps
+  // React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, indicatorColor = "bg-primary", ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +24,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn("h-full w-full flex-1 transition-all", indicatorColor)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
